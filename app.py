230491aa59d69538346bc89474fdcb703725ef86
Leonan <leonan@exemplo.com>
2026-03-16 16:52:28 -0300

    Commit inicial

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # O Flask procura o arquivo dentro da pasta /templates
     return render_template('index.html')
 
+# Rota para servir o manifesto na raiz
+@app.route('/manifest.json')
+def manifest():
+    return send_from_directory('static', 'manifest.json')
+
+# Rota para servir o service worker na raiz
+@app.route('/sw.js')
+def sw():
+    return send_from_directory('static', 'sw.js')
+
 if __name__ == '__main__':
-    # A porta é definida automaticamente pelo servidor de hospedagem
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
