1ef2b625670037a1b6f30386897009ca0baa90ef
Leonan <leonan@exemplo.com>
2026-03-17 03:11:18 -0300

    Corrigido caminho da logo no index.html

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # O Flask busca automaticamente na pasta 'templates'
     return render_template('index.html')
 
 # Rota para servir o manifesto na raiz
@@ -18,5 +19,6 @@ def sw():
     return send_from_directory('static', 'sw.js')
 
 if __name__ == '__main__':
+    # Porta configurada para rodar localmente ou em servidores como Render/Heroku
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
